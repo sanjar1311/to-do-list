867a7cc6abe4c597f8aba3e8de2ae0a37b47476e
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2020-12-14 22:26:09 +0500

    fixed include funcsion

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var elInput = elForm.querySelector('.js-input');
 var elCheckbox = elForm.querySelector('.js-checkbox');
 var toDo = document.querySelector('.js-todo');
 var elOutput = document.querySelector('.js-output');
+var elIsInclude = document.querySelector('.js-isInclude-output');
 
 var toDoList = [];
 
@@ -10,23 +11,30 @@ elForm.addEventListener('submit', function(evt) {
   evt.preventDefault();
   var isChecked = elCheckbox.checked;
 
-  var myFunction = function() {
+  if(toDoList.includes(elInput.value)) {
+    elIsInclude.classList.add('text-center','h4','text-danger');
+    elIsInclude.textContent = 'Error';
+    return
+  }else{
+    elIsInclude.textContent = "";
+    var myFunction = function() {
+      if(isChecked) {
+        toDoList.unshift(elInput.value);
+        elOutput.innerHTML = `<p> ${toDoList.join('<p class="border-top"></p>')} </p>`;
+        elInput.value = '';
+      }
+    }
+    elCheckbox.addEventListener('change', function(){
+      myFunction();
+    });
+
     if(isChecked) {
-      toDoList.unshift(elInput.value);
-      elOutput.innerHTML = `<p> ${toDoList.join('<p class="border-top"></p>')} </p>`;
+      myFunction();
+    }else{
+      toDoList.push(elInput.value);
+      elOutput.classList.add('border-bottom');
+      elOutput.innerHTML = `<p class="m-0"> ${toDoList.join('<p class="border-top"></p>')} </p>`;
       elInput.value = '';
     }
-  }
-  elCheckbox.addEventListener('change', function(){
-    myFunction();
-  });
-
-  if(isChecked) {
-    myFunction();
-  }else{
-    toDoList.push(elInput.value);
-    elOutput.classList.add('border-bottom');
-    elOutput.innerHTML = `<p class="m-0"> ${toDoList.join('<p class="border-top"></p>')} </p>`;
-    elInput.value = '';
   }
 });
